5df2299a8d143a982b36ac1f5d18b13560d61eef
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-01 18:12:38 +0000

    Update main.js

--- scr/js/main.js
+++ scr/js/main.js
@@ -3,7 +3,7 @@ import { init } from './submain.js';
 import * as EventLogAndSchedule from './eventLogAndSchedule.js';
 import * as StandingsAndBracket from './standingsAndBracket.js';
 import * as TeamManagement from './teamManagement.js';
-import * as Achievements from './achievements.js'; 
+import * as Achievements from './achievementsAndWiki.js'; 
 import * as Participants from './participants.js';
 import { SaveSystem } from './saveSystem.js'; // Importación Corregida
 import * as ControlsAndCompatibility from './controlsAndCompatibility.js';
