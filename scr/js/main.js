0e51e3c784ab1c099d6b9dcf5b394846172001a7
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-01 17:43:37 +0000

    Update main.js

--- scr/js/main.js
+++ scr/js/main.js
@@ -1,12 +1,12 @@
-// main.js
+// scr/js/main.js
+import { init } from './submain.js';
 import * as EventLogAndSchedule from './eventLogAndSchedule.js';
 import * as StandingsAndBracket from './standingsAndBracket.js';
 import * as TeamManagement from './teamManagement.js';
-import * as AchievementsAndWiki from './achievementsAndWiki.js';
+import * as Achievements from './achievements.js'; // Importación actualizada
 import * as Participants from './participants.js';
 import * as SaveSystem from './saveSystem.js';
 import * as ControlsAndCompatibility from './controlsAndCompatibility.js';
-import { init } from './submain.js';
 
 // Inicializa el juego al cargar la página
 window.onload = () => {
@@ -14,7 +14,7 @@ window.onload = () => {
         EventLogAndSchedule,
         StandingsAndBracket,
         TeamManagement,
-        AchievementsAndWiki,
+        Achievements, // Módulo renombrado
         Participants,
         SaveSystem,
         ControlsAndCompatibility
