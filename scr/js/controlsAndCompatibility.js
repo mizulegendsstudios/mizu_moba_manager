fe0c31514fa4a6b195ef378bf780c0b01e4cee55
Moises Núñez Ibarra <mizulegendsgg@gmail.com>
2025-09-01 17:47:16 +0000

    Update controlsAndCompatibility.js

--- scr/js/controlsAndCompatibility.js
+++ scr/js/controlsAndCompatibility.js
@@ -1,28 +1,45 @@
-// controlsAndCompatibility.js
+// scr/js/controlsAndCompatibility.js
 const ControlsAndCompatibility = (function() {
-    // Configuración de controles
-    let controls = {
+    // Configuración de controles por defecto
+    const DEFAULT_CONTROLS = {
         select: 'Enter',
         back: 'Escape',
-        // Agrega más controles según sea necesario
+        simulate: 'S',
     };
+
+    let controls = {};
+
+    // Carga los controles desde un guardado o usa los por defecto
+    function loadControls(data) {
+        if (data && data.controls) {
+            controls = data.controls;
+        } else {
+            controls = DEFAULT_CONTROLS;
+        }
+    }
+    
+    // Obtiene la configuración de controles actual
+    function getControls() {
+        return controls;
+    }
+
+    // Actualiza un control específico y lo guarda
+    function setControl(action, key) {
+        controls[action] = key;
+    }
 
     // Verifica la compatibilidad del navegador
     function checkCompatibility() {
         return {
             localStorage: typeof(Storage) !== 'undefined',
-            // Agrega más verificaciones según sea necesario
         };
     }
 
-    // Obtiene la configuración de controles
-    function getControls() {
-        return controls;
-    }
-
     return {
-        checkCompatibility,
-        getControls
+        loadControls,
+        getControls,
+        setControl,
+        checkCompatibility
     };
 })();
 
